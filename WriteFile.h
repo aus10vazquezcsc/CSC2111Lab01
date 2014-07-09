92f7cb0ff6c950f30b171ca944e3ada6b58da1ed
ggrebello <ggomesreb42@students.tntech.edu>
2014-07-09 15:06:52 -0500

    I put the comments similar to Tokens.h

--- WriteFile.h
+++ WriteFile.h
@@ -6,16 +6,27 @@
 #include <fstream>
 using namespace std;
 
+//Class to start writing into a text file
+
+/*
+* The methods consist in create a text file,
+* open, write and close it
+*/
+
 class WriteFile
 {
    private:
     ofstream output_file;
-    bool closed;
+    bool closed;				//determines if the file can be written or not
    
    public:
+   //! The constructor that opens a text file to be written
     WriteFile(const char* file_name);
+   //! The destructor deletes all the variables
 	virtual ~WriteFile();
+	//! This method writes some text from the user into the file
 	void writeLine(string* line);
+	//! This closes the text and ends the writing
 	void close();
 
 };
