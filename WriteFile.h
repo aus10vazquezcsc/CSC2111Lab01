a9f9291f4de5158f387dde6b5e158c8c2f506f46
Macedonio Austin Vazquez <vazquez.austin@yahoo.com>
2014-07-09 22:11:44 -0500

    Fixed merge

--- WriteFile.h
+++ WriteFile.h
@@ -15,35 +15,18 @@ using namespace std;
 
 class WriteFile
 {
-<<<<<<< HEAD
     private:
         ofstream output_file;
         bool closed;
 	public:
-    	WriteFile(const char* file_name);
+	    //! The constructor that opens a text file to be written
+    	WriteFile(const char* file_name);  
+		//! The destructor deletes all the variables
         ~WriteFile();
+		//! This method writes some text from the user into the file
         void writeLine(String* line);
+		//! This closes the text and ends the writing
         void close();
 };
 
-=======
-   private:
-    ofstream output_file;
-    bool closed;				//determines if the file can be written or not
-   
-   public:
-   //! The constructor that opens a text file to be written
-    WriteFile(const char* file_name);
-   //! The destructor deletes all the variables
-	virtual ~WriteFile();
-	//! This method writes some text from the user into the file
-	void writeLine(string* line);
-	//! This closes the text and ends the writing
-	void close();
-
-};
-
-
-
->>>>>>> 1280d1f6b2f7ee0857c95d2e221ff41cdf16108b
 #endif
